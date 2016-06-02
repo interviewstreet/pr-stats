e7736ddf8acaeeb1f628a4e7ab343e0de076570e
Rishi <rishiloyola98245@gmail.com>
2016-06-02 15:03:55 +0530

    Update main.go

--- main.go
+++ main.go
@@ -4,20 +4,22 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
+	"strconv"
+	"strings"
 	"sync"
 	"time"
-	"strconv"
+	"runtime"
 
 	"github.com/google/go-github/github"
 	"golang.org/x/oauth2"
-	"gopkg.in/olivere/elastic.v3"
 	"gopkg.in/yaml.v2"
 )
 
 type Data struct {
-	mDays int   `json:"mDays"`
-	nDays int   `json:"nDays"`
-	prID  int   `json:"prID"`
+	commentDuration int `json:"commentDuration"`
+	submitDuration  int `json:"submitDuration"`
+	prID            int `json:"prID"`
 }
 
 type Config struct {
@@ -30,61 +32,46 @@ type Config struct {
 		Start string `yaml:"Start"`
 		End   string `yaml:"End"`
 	} `yaml:"TimeLine"`
-	Elasticsearch string `yaml:"Elasticsearch"`
 }
 
 type agent struct {
-	config    Config
-	startTime time.Time
-	endTime   time.Time
-	client    *github.Client
-	esClient  *elastic.Client
+	config      Config
+	startTime   time.Time
+	endTime     time.Time
+	client      *github.Client
+	currentTime time.Time
+	file        *os.File
 }
 
 func New() *agent {
 
 	a := &agent{}
 	source, err := ioutil.ReadFile("config.yaml")
-	if err != nil {
-		panic(err)
-	}
+	check(err)
 
 	err = yaml.Unmarshal(source, &(a.config))
-	if err != nil {
-		panic(err)
-	}
+	check(err)
 
-	fmt.Printf("Your token value: %#v\n", a.config.TimeLine.Start)
+	fmt.Printf("Your token value: %#v\n", a.config.AccessToken)
 
 	a.startTime, err = time.Parse("2006-01-02", a.config.TimeLine.Start)
-	if err != nil {
-		panic(err)
-	}
+	check(err)
+
 	a.endTime, err = time.Parse("2006-01-02", a.config.TimeLine.End)
-	if err != nil {
-		panic(err)
-	}
+	check(err)
 
 	ts := oauth2.StaticTokenSource(
 		&oauth2.Token{AccessToken: a.config.AccessToken},
 	)
 	tc := oauth2.NewClient(oauth2.NoContext, ts)
-
 	a.client = github.NewClient(tc)
 
-	a.esClient, err = elastic.NewClient(
-		elastic.SetURL(a.config.Elasticsearch),
-		elastic.SetSniff(false),
-	)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(a.config.Elasticsearch)
-	info, code, err := a.esClient.Ping(a.config.Elasticsearch).Do()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("Elasticsearch returned with code %d and version %s", code, info.Version.Number)
+	currentTime := time.Now()
+	currentTime.Format("2006-01-02")
+	a.currentTime = currentTime
+
+	a.file, err = os.Create("/Users/Rishi/.gvm/pkgsets/go1.4/global/src/github.com/rishiloyola/pr-stats/github.json")
+	check(err)
 
 	return a
 }
@@ -95,121 +82,148 @@ func main() {
 		breakLoop bool
 		wg        sync.WaitGroup
 	)
-	a := New()
 
+	a := New()
 	opt := &github.PullRequestListOptions{
-		ListOptions: github.ListOptions{PerPage: 1000},
+		ListOptions: github.ListOptions{PerPage: 100},
 		State:       "all",
 	}
 	breakLoop = false
 
 	for {
 		prs, resp, err := a.client.PullRequests.List(a.config.Search.User, a.config.Search.Repo, opt)
-
+		
 		if _, ok := err.(*github.RateLimitError); ok {
-			log.Println("hit rate limit. Try after an hour.")
-		} else if err != nil {
-			panic(err)
+			log.Println("Hit rate limit. Try after an hour.")
+		} else {
+			check(err)
 		}
+
+		for i := 0; i < len(prs); i++ {
+			current := *prs[i].CreatedAt
+
+			if current.Before(a.startTime) {
+				if current.Before(a.endTime) {
+					breakLoop = true
+					break
+					fmt.Println("break")
+				}
+				wg.Add(1)
+
+				go a.process(current, prs[i].Number, prs[i].ClosedAt, prs[i].MergedAt, getName(prs[i].User), getName(prs[i].Assignee), prs[i].State, &wg)
+				time.Sleep(1000 * time.Millisecond)
+			}
+		}
+
 		if resp.NextPage == 0 {
 			break
 		}
 
 		opt.ListOptions.Page = resp.NextPage
 
-		for i := 0; i < len(prs); i++ {
-			current := *prs[i].CreatedAt
-			if current.Before(a.endTime) {
-				if current.Before(a.startTime) {
-					breakLoop = true
-					break
-				}
-				wg.Add(1)
-				go a.process(current, prs[i].Number, prs[i].ClosedAt, prs[i].MergedAt, &wg)
-			}
-		}
-
 		if breakLoop {
 			break
 		}
 	}
+
 	wg.Wait()
+	defer a.file.Close()
 }
 
-func (a *agent) process(createdAt time.Time, id *int, closedAt *time.Time, mergedAt *time.Time, wg *sync.WaitGroup) {
+func (a *agent) process(createdAt time.Time, id *int, closedAt *time.Time, mergedAt *time.Time, user string, assignee string, status *string, wg *sync.WaitGroup) {
+
 	var (
 		commentCreatedAt time.Time
-		mDays            time.Duration
-		nDays            time.Duration
+		commentDuration  time.Duration
+		submitDuration   time.Duration
 	)
 
-	opt := &github.IssueListCommentsOptions{
+	optComments := &github.IssueListCommentsOptions{
 		ListOptions: github.ListOptions{PerPage: 2},
 	}
-	comments, _, err := a.client.Issues.ListComments(a.config.Search.User, a.config.Search.Repo, *id, opt)
+	comments, _, err := a.client.Issues.ListComments(a.config.Search.User, a.config.Search.Repo, *id, optComments)
+	// optLabels := &github.ListOptions{
+	// 	Page: 1,
+	// 	PerPage: 10,
+	// }
+	// labels, _, err := a.client.Issues.ListLabelsByIssue(a.config.Search.User, a.config.Search.Repo, *id, optLabels)
 
 	if _, ok := err.(*github.RateLimitError); ok {
 		log.Println("hit rate limit. Try after an hour.")
-	} else if err != nil {
-		panic(err)
+		os.Exit(0)
+	} else {
+		check(err)
 	}
 
 	if len(comments) > 0 {
 		commentCreatedAt = *comments[0].CreatedAt
-		mDays = commentCreatedAt.Sub(createdAt)
+		commentDuration = commentCreatedAt.Sub(createdAt)
+
 		if closedAt != nil {
-			nDays = closedAt.Sub(commentCreatedAt)
+			submitDuration = closedAt.Sub(commentCreatedAt)
 		} else if mergedAt != nil {
-			nDays = mergedAt.Sub(commentCreatedAt)
+			submitDuration = mergedAt.Sub(commentCreatedAt)
 		} else {
 			if a.endTime.Sub(commentCreatedAt) > 0 {
-				nDays = a.endTime.Sub(commentCreatedAt)
+				submitDuration = a.currentTime.Sub(commentCreatedAt)
 			} else {
-				nDays = commentCreatedAt.Sub(a.endTime)
+				submitDuration = commentCreatedAt.Sub(a.endTime)
 			} //negative
 		}
+
 	} else {
+
 		if closedAt != nil {
-			mDays = 0
-			nDays = closedAt.Sub(createdAt)
+			commentDuration = 0
+			submitDuration = closedAt.Sub(createdAt)
 		} else if mergedAt != nil {
-			mDays = 0
-			nDays = mergedAt.Sub(createdAt)
+			commentDuration = 0
+			submitDuration = mergedAt.Sub(createdAt)
 		} else {
-			mDays = a.endTime.Sub(createdAt)
-			nDays = 0
+			commentDuration = a.currentTime.Sub(createdAt)
+			submitDuration = 0
 		}
-	}
-	//fmt.Printf("mdays : %v  ndays : %v", int(mDays.Hours()/24), int(nDays.Hours()/24))
-	//fmt.Println()
 
-	exists, err := a.esClient.IndexExists("test").Do()
-	if err != nil {
-		panic(err)
 	}
-	if !exists {
-		// Create a new index.
-		_, err := a.esClient.CreateIndex("test").Do()
-		if err != nil {
-			panic(err)
-		}
-	}
-	_mDays := strconv.Itoa(int(mDays.Hours()/24))
-	_nDays := strconv.Itoa(int(nDays.Hours()/24))
-	_id := strconv.Itoa(*id)
 
-	// Index a tweet (using JSON serialization)
-	message := `{"mDays": `+ _mDays +`, "nDays": `+ _nDays +`, "prID": `+ _id +`}`
+	_commentDuration := strconv.Itoa(int(commentDuration.Hours() / 24))
+	_submitDuration := strconv.Itoa(int(submitDuration.Hours() / 24))
+	_prID := strconv.Itoa(*id)
+	created_on := strings.Split(createdAt.String(), " ")
+
+	message := `{` +
+		`"commentDuration": ` + _commentDuration +
+		`, "submitDuration": ` + _submitDuration +
+		`, "prID": "` + _prID +
+		`", "status": "` + (*status) +
+		`", "assignee": "` + assignee +
+		`", "createdBy": "` + user +
+		`", "created_on": "` + created_on[0] +
+		`"}`
+
+	_, err = a.file.Write([]byte(message + "\n"))
+	check(err)
+
 	fmt.Println(message)
-	put1, err := a.esClient.Index().
-		Index("test").
-		Type("PullRequests").
-		Id(_id).
-		BodyJson(message).
-		Do()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("Indexed message %s to index %s, type %s\n", put1.Id, put1.Index, put1.Type)
+
 	wg.Done()
+}
+
+func getName(user *github.User) string {
+
+	if user != nil {
+		return *user.Login
+	} else {
+		return "null"
+	}
+
+}
+
+func check(err error) {
+	if err != nil {
+		pc, _, line, _ := runtime.Caller(1)
+    	message := fmt.Sprintf("[error] %s[%d] %s", runtime.FuncForPC(pc).Name(), line, err.Error())
+    	log.Println(message)
+    	os.Exit(0)
+	}
 }
